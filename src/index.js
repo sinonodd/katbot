b04f3620d31e710dd8cb124d6c71a04ba96eb303
sinonodd <kamal8aly545@gmail.com>
2021-01-20 18:03:00 +0100

    :sparkles: Yeey my bot is responding :sunglasses:

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config();
 
+const guildID = '801422578471403551';
+const channelID = '801422579235684374';
 client.once('ready', () => {
-    console.log('whatever you saying');
+    console.log('beep boop!');
+});
+client.on('message', async (msg) => {
+    if (msg.guild.id === guildID && msg.channel.id === channelID) {
+        if (msg.content.toLowerCase() === 'bot') {
+            await msg.reply('Yes master!');
+            console.log('sent');
+        }
+    }
 });
 client.login(process.env.BOT_TOKEN);
